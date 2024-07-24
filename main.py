daca1faca4fd6853ca739214a9475a19d5e154c5
rahul0715 <ajaychouhan26367@gmail.com>
2024-07-24 18:23:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ from tglogging import TelegramLogHandler
 
 # Config 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6258566897:AAHABxcch0ROkLGzQlNA5XStSoZifE2llEE")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6611654088:AAE_ACVRTLoucGh_YpnJcuqauyRI3c1cHbw")
     API_ID = int(os.environ.get("API_ID",  "22505271"))
     API_HASH = os.environ.get("API_HASH", "c89a94fcfda4bc06524d0903977fc81e")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     SESSIONS = "./SESSIONS"
 
-    AUTH_USERS = os.environ.get('AUTH_USERS', '1871785273').split(',')
+    AUTH_USERS = os.environ.get('AUTH_USERS', '5881684718').split(',')
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
-    GROUPS = os.environ.get('GROUPS', '-1002064268488').split(',')
+    GROUPS = os.environ.get('GROUPS', '-1002207321653').split(',')
     for i in range(len(GROUPS)):
         GROUPS[i] = int(GROUPS[i])
 
-    LOG_CH = os.environ.get("LOG_CH", "-1002132785565")
+    LOG_CH = os.environ.get("LOG_CH", "-1002182529805")
 
 # TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
 logging.basicConfig(
